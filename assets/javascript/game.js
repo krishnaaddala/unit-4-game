a93abaf88958bd86938b6ab4d7b9313c02ff1a77
Krishna <kaddala@salesforce.com>
2019-10-19 22:26:29 -0700

    changes and modification to the game logic and adding pseudo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,69 @@
 //create global variables for computer generated and hidden values for each crystal and also for wins and loses
 var computerChoice = Math.floor(Math.random() * ((120-19)+1) + 19);
-console.log(computerChoice);
-// var crystalNumber1 = Math.floor(Math.random() * 12) + 1;
-// var crystalNumber2 = Math.floor(Math.random() * 12) + 1;
-// var crystalNumber3 = Math.floor(Math.random() * 12) + 1;
-// var crystalNumber4 = Math.floor(Math.random() * 12) + 1;
-
-//console.log(crystalNumber);
-// create a array list of images to associate a random crystalNumber
-var imagesList = ["assets/images/blue.png", "assets/images/green.png", "assets/images/red.png", "assets/images/yellow.png"];
+//create 4 crystal num's one for each crystal
+var crystalNumber1 = Math.floor(Math.random() * 12) + 1;
+var crystalNumber2 = Math.floor(Math.random() * 12) + 1;
+var crystalNumber3 = Math.floor(Math.random() * 12) + 1;
+var crystalNumber4 = Math.floor(Math.random() * 12) + 1;
+//create variables wins, loses and allCrystal and set them to 0 as initial value
 var wins = 0;
 var loses = 0;
-var crystalClick = 0;
-var numberOfWins = document.getElementById("wins");
-var numberOfLosses = document.getElementById("loses");
-var youWin = document.getElementById("youWin");
-var youLost = document.getElementById("youLost");
+var allCrystalValue = 0;
+//Function for reset the game after win/loss
 function resetGame(){
      computerChoice = Math.floor(Math.random() * ((120-19)+1) + 19);
      $("#computerRandom").text(computerChoice);
-     randCrystalNum();
-    //  crystalNumber1 = Math.floor(Math.random() * 12) + 1;
-    //  crystalNumber2 = Math.floor(Math.random() * 12) + 1;
-    //  crystalNumber3 = Math.floor(Math.random() * 12) + 1;
-    //  crystalNumber4 = Math.floor(Math.random() * 12) + 1;
-}
-function randCrystalNum(){
-for (i=1;i<=4;i++){
-    crystalClick[i] = Math.floor(Math.random() * 12) + 1;
-}
-};
-//create clicks for 4 crystal images
-$("#computerRandom").text(computerChoice);
+     crystalNumber1 = Math.floor(Math.random() * 12) + 1;
+     crystalNumber2 = Math.floor(Math.random() * 12) + 1;
+     crystalNumber3 = Math.floor(Math.random() * 12) + 1;
+     crystalNumber4 = Math.floor(Math.random() * 12) + 1;
+    allCrystalValue =0;
 
-$("#crystalimages").on("click", function(){
-    var crystalValue1 = imagesList[randCrystalNum()];
-    console.log(crystalValue1)
-    // var crystalValue2 = imagesList[crystalNumber2];
-    // var crystalValue3 = imagesList[crystalNumber3];
-    // var crystalValue4 = imagesList[crystalNumber4];
-    var allCrystalsValue = parseInt(crystalValue1);
-        //+ crystalValue2+crystalValue3+crystalValue4);
-    crystalClick+=allCrystalsValue;
-    if (crystalClick === computerChoice){
-        $("#youWin").text("You Win!")
-        wins++;
-        resetGame();
-    }
-    else{
-        $("#youlost").text("You Lost!")
-        loses++;
-        resetGame();
-    }
+}
+//This will place the computer generated random number on the page using the ID
+$("#computerRandom").text(computerChoice);
+//similarly the below will set the values for wins/loses to 0 and gameStatus to BLANK
+$("#winsCount").text(0);
+$("#losesCount").text(0);
+$("#gameStatus").text("");
+
+//create clicks for 4 crystal images
+$("#blue-image").on("click", function(){
+    allCrystalValue = allCrystalValue+crystalNumber1;
+    $("#totalScoreNumber").text(allCrystalValue);
+    checkGameStatus();
 });
-numberOfWins.textContent = "Wins :" + wins;
-numberOfLosses.textContent = "Loses :" + loses;
+$("#green-image").on("click", function(){
+    allCrystalValue = allCrystalValue+crystalNumber2;
+    $("#totalScoreNumber").text(allCrystalValue);
+    checkGameStatus();
+});
+$("#red-image").on("click", function(){
+    allCrystalValue = allCrystalValue+crystalNumber3;
+    $("#totalScoreNumber").text(allCrystalValue);
+    checkGameStatus();
+});
+$("#yellow-image").on("click", function(){
+    allCrystalValue = allCrystalValue+crystalNumber4;
+    $("#totalScoreNumber").text(allCrystalValue);
+    checkGameStatus();
+});
+
+//function for checking Game Status comparing with computer choice and declare win/loss
+function checkGameStatus(){
+    if (allCrystalValue === computerChoice){
+        $("#gameStatus").text("You Win!")
+        wins++;
+        $("#winsCount").text(wins);
+        resetGame();
+    }
+    else if (allCrystalValue > computerChoice){
+        $("#gameStatus").text("You Lost!")
+        loses++;
+        $("#losesCount").text(loses);
+        resetGame();
+    }
+    else {
+        $("#gameStatus").text("");
+    }
+}

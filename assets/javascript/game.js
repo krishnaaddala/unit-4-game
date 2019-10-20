17bd55cab52fabadce5eb2ee8be4d15d59d5801b
Krishna <kaddala@salesforce.com>
2019-10-19 18:52:46 -0700

    trail and error with the ganme logic to see how we can add number value to each of the crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 //create global variables for computer generated and hidden values for each crystal and also for wins and loses
 var computerChoice = Math.floor(Math.random() * ((120-19)+1) + 19);
 console.log(computerChoice);
-var crystalNumber1 = Math.floor(Math.random() * 12) + 1;
-var crystalNumber2 = Math.floor(Math.random() * 12) + 1;
-var crystalNumber3 = Math.floor(Math.random() * 12) + 1;
-var crystalNumber4 = Math.floor(Math.random() * 12) + 1;
+// var crystalNumber1 = Math.floor(Math.random() * 12) + 1;
+// var crystalNumber2 = Math.floor(Math.random() * 12) + 1;
+// var crystalNumber3 = Math.floor(Math.random() * 12) + 1;
+// var crystalNumber4 = Math.floor(Math.random() * 12) + 1;
 
 //console.log(crystalNumber);
 // create a array list of images to associate a random crystalNumber
-var imagesList = ["blue.png", "green.png", "red.png", "yellow.png"];
+var imagesList = ["assets/images/blue.png", "assets/images/green.png", "assets/images/red.png", "assets/images/yellow.png"];
 var wins = 0;
 var loses = 0;
 var crystalClick = 0;
@@ -18,23 +18,29 @@ var youWin = document.getElementById("youWin");
 var youLost = document.getElementById("youLost");
 function resetGame(){
      computerChoice = Math.floor(Math.random() * ((120-19)+1) + 19);
-     crystalNumber = Math.floor(Math.random() * 12) + 1;
      $("#computerRandom").text(computerChoice);
-     crystalNumber1 = Math.floor(Math.random() * 12) + 1;
-     crystalNumber2 = Math.floor(Math.random() * 12) + 1;
-     crystalNumber3 = Math.floor(Math.random() * 12) + 1;
-     crystalNumber4 = Math.floor(Math.random() * 12) + 1;
-
+     randCrystalNum();
+    //  crystalNumber1 = Math.floor(Math.random() * 12) + 1;
+    //  crystalNumber2 = Math.floor(Math.random() * 12) + 1;
+    //  crystalNumber3 = Math.floor(Math.random() * 12) + 1;
+    //  crystalNumber4 = Math.floor(Math.random() * 12) + 1;
 }
+function randCrystalNum(){
+for (i=1;i<=4;i++){
+    crystalClick[i] = Math.floor(Math.random() * 12) + 1;
+}
+};
 //create clicks for 4 crystal images
 $("#computerRandom").text(computerChoice);
 
 $("#crystalimages").on("click", function(){
-    var crystalValue1 = crystalNumber1[imagesList[0]];
-    var crystalValue2 = crystalNumber2[imagesList[1]];
-    var crystalValue3 = crystalNumber3[imagesList[2]];
-    var crystalValue4 = crystalNumber4[imagesList[3]];
-    var allCrystalsValue = crystalValue1+ crystalValue2+crystalValue3+crystalValue4;
+    var crystalValue1 = imagesList[randCrystalNum()];
+    console.log(crystalValue1)
+    // var crystalValue2 = imagesList[crystalNumber2];
+    // var crystalValue3 = imagesList[crystalNumber3];
+    // var crystalValue4 = imagesList[crystalNumber4];
+    var allCrystalsValue = parseInt(crystalValue1);
+        //+ crystalValue2+crystalValue3+crystalValue4);
     crystalClick+=allCrystalsValue;
     if (crystalClick === computerChoice){
         $("#youWin").text("You Win!")
